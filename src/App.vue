1185036f77763c1e02097c42332f73d9a8259731
xiaodongqi <731310612@qq.com>
2019-06-04 10:44:47 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="container">
+    <search/>
+    <Main/>
+  </div>
+</template>
+
+<script>
+import Main from "./components/Main.vue";
+import Search from "./components/Search.vue";
+
+export default {
+  components: {
+    Main,
+    Search
+  }
+};
+</script>
+
+<style>
+.card {
+  float: left;
+  width: 33.333%;
+  padding: 0.75rem;
+  margin-bottom: 2rem;
+  border: 1px solid #efefef;
+  text-align: center;
+}
+.card > img {
+  margin-bottom: 0.75rem;
+  border-radius: 100px;
+}
+.card-text {
+  font-size: 85%;
+}
+</style>
